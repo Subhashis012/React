185e15752c17855eaaceda13e35658d6b8b2353b
Subhashis <subhashisdhara123@gmail.com>
2024-09-05 00:42:43 +0530

    added

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,11 +3,13 @@ import Chai  from "./chai"
 import S from './s.jsx'
 
 function App() {
+
+  const username = "subhashis"
   
   return (
     <>
     <Chai />
-    <h1>Chai aur react</h1>
+    <h1>Chai aur react {username}</h1>
     <p>test para</p>
     <S />
     </>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import S from './s.jsx'
+
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://goggle.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href='https://goggle.com' target='_blank'>Click me to visit google</a>
+)
+
+const anotherUser = "chai aurr react"
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://goggle.com',
+        target: '_blank'
+    },
+    'Click me to visit google',
+    anotherUser
+)
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     
-    <>
-    <App />
-    <S />
-    </>
-    
+   reactElement
+
 )
