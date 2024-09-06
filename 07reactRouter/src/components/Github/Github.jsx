5a824d4c5721e33c8a9d2d9d09b4f6b81a701501
Subhashis <subhashisdhara123@gmail.com>
2024-09-07 01:16:55 +0530

    added new

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/Subhashis012')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // })
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github Followers: {data.followers}
+      <img className='rounded-full flex justify-center items-center' src={data.avatar_url} alt="avatar" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/Subhashis012')
+    return response.json()
+}
